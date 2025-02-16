9f4a374ab65ef8dff3a434cf9426a4924bba159b
rhit-flannacj <119346129+rhit-flannacj@users.noreply.github.com>
2025-02-16 16:06:41 -0500

    Newest

--- src/main/java/org/team3/UMLDisplay.java
+++ src/main/java/org/team3/UMLDisplay.java
@@ -13,21 +13,21 @@ import java.nio.file.StandardCopyOption;
 
 public class UMLDisplay {
     public void renderUML(String uml) throws Exception {
-        String save = "C:\\Users\\flannacj\\Downloads\\uml.svg";
+        String save = System.getProperty("user.dir") + "\\src\\main\\Output\\uml.svg";
         Encoder encoder = new Encoder();
         URL url = new URL(encoder.encode(uml));
         System.out.println(url);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
-        if (connection.getResponseCode() == 200) {
-            try (InputStream inputStream = connection.getInputStream();
-            FileOutputStream fileOutputStream = new FileOutputStream(save)) {
-                byte[] buffer = new byte[1024];
-                int bytesRead = 0;
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    fileOutputStream.write(buffer, 0, bytesRead);
-                }
-            }
-        }
+//        if (connection.getResponseCode() == 200) {
+//            try (InputStream inputStream = connection.getInputStream();
+//            FileOutputStream fileOutputStream = new FileOutputStream(save)) {
+//                byte[] buffer = new byte[1024];
+//                int bytesRead = 0;
+//                while ((bytesRead = inputStream.read(buffer)) != -1) {
+//                    fileOutputStream.write(buffer, 0, bytesRead);
+//                }
+//            }
+//        }
     }
 }
